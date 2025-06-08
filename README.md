ba7a9bbc3e455a2912d75c5e53759e5c94b5d55d
Alejandro Valdez <alexsvaldez2011@gmail.com>
2025-06-08 15:27:50 -0500

    rewrote a whole bunch of stuff
    Finally added the math that makes this guess the prices, had to redo it several times. It now guesses the start date with a rough estimation, and its discount / price. Moved some stuff that doesn't need to be on Github to a test folder and added it to .gitignore.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Steam Sale Guesser
+___
+
+This is a project that aims to guess a Steam app / game price using its Steam appID. It does this by fetching data from the Steam API as well as getting some data from SteamDB for the price history.
--- index.js
+++ index.js
@@ -1,62 +1,78 @@
-async function readFile(filePath) {
-  console.log(`Reading file from: ${filePath}`);
-  const response = await fetch(filePath);
-  if (!response.ok) {
-    throw new Error(`Error thrown: ${response.stauts}`);
-  }
-  const data = await response.json();
-  return data;
+async function fetchData(filePath) {
+	console.log(`Fetching data from: ${filePath}`);
+	const response = await fetch(filePath);
+	if (!response.ok) {
+		return response;
+	}
+	console.log(`Status code from '${filePath.hostname}' : ${response.status}`);
+	const data = await response.json();
+	return data;
 }
 
 function calculateInitialPrice(num, percent) {
-  return num / (1 - percent / 100);
+	return num / (1 - percent / 100);
 }
 
 function roundToNearestTenth(number) {
-  return Math.ceil(number * 100) / 100;
+	return Math.round(number * 100) / 100;
 }
 
-function returnSaleName(input) {
-  if (input === undefined) {
-    return "";
-  } else {
-    return `\n${input}`;
-  }
+function styleDate(date) {
+	return new Intl.DateTimeFormat(undefined, { dateStyle: "full" }).format(
+		date
+	);
 }
 
-function checkForLastSale(StartDate, EndDate) {
-  if (StartDate.d === undefined) {
-    return "This dataset is invalid, please retry with a valid set.";
-  } else {
-    if (StartDate.d == 0) {
-      return "A sale is currently going on";
+function checkForLastSale(dataSet) {
+    const data = dataSet.data.history; 
+    const StartDate = data[data.length - 2];
+    const EndDate = data[data.length - 1];
+    //console.log(data[data.length - 3]);
+    if (StartDate.d === undefined) {
+        return "This dataset is invalid, please retry with a valid set.";
     } else {
-      return `The last sale lasted for ${DaysBetween(
-        StartDate.x,
-        EndDate.x
-      )}, with a discount of ${StartDate.d}% off`;
+        if (StartDate.d == 0) {
+            return "A sale is currently going on";
+        } else {
+            var daysInBetween = DaysBetween(StartDate.x, EndDate.x);
+            console.log(
+                `The last sale lasted for ${daysInBetween}, with a discount of ${StartDate.d}% off\nThe time between the previous sale was ${DaysBetween(data[data.length - 3].x,StartDate.x)} days or ${DaysBetween(data[data.length - 3].x, StartDate.x) / 7} weeks${DaysBetween(data[data.length - 3].x, StartDate.x) % 7 !== 0?` and ${DaysBetween(data[data.length - 3].x,StartDate.x) % 7}`:""}.`
+              );
+            var fixed = new Date(EndDate.x);
+            console.log(fixed)
+            if (StartDate) {
+
+            }
+            console.log(
+                `The next sale is estimated to start on ${styleDate(new Date(fixed.setDate(fixed.getDate()+DaysBetween(data[data.length - 3].x,StartDate.x))))} or in ${DaysBetween(new Date(),new Date(new Date().setDate(new Date().getDate() + DaysBetween(data[data.length - 3].x,StartDate.x))))} days.`
+            );
+            const discountAvg = (StartDate.d + data[data.length - 4].d + data[data.length - 6].d + data[data.length - 8].d) / 4;
+			console.log(DaysBetween(data[data.length - 5].x,data[data.length - 4].x));
+			const timeBetweenSalesAvg = (DaysBetween(data[data.length - 3].x,StartDate.x)+DaysBetween(data[data.length - 5].x,data[data.length - 4].x))/2;
+            console.log(discountAvg);
+            return JSON.parse(`{"saleDuration": ${daysInBetween}, "timeBetweenSales": ${timeBetweenSalesAvg}, "priceEstimation": ${roundToNearestTenth(EndDate.y * (1-(discountAvg/100)))}, "discountEstimation": ${discountAvg}}`);
+        }
     }
-  }
 }
 
 function DaysBetween(StartDate, EndDate) {
-  // The number of milliseconds in all UTC days (no DST)
-  const oneDay = 1000 * 60 * 60 * 24;
+	// The number of milliseconds in all UTC days (no DST)
+	const oneDay = 1000 * 60 * 60 * 24;
 
-  // A day in UTC always lasts 24 hours (unlike in other time formats)
-  const start = Date.UTC(
-    new Date(EndDate).getFullYear(),
-    new Date(EndDate).getMonth(),
-    new Date(EndDate).getDate()
-  );
-  const end = Date.UTC(
-    new Date(StartDate).getFullYear(),
-    new Date(StartDate).getMonth(),
-    new Date(StartDate).getDate()
-  );
+	// A day in UTC always lasts 24 hours (unlike in other time formats)
+	const start = Date.UTC(
+		new Date(EndDate).getFullYear(),
+		new Date(EndDate).getMonth(),
+		new Date(EndDate).getDate()
+	);
+	const end = Date.UTC(
+		new Date(StartDate).getFullYear(),
+		new Date(StartDate).getMonth(),
+		new Date(StartDate).getDate()
+	);
 
-  // so it's safe to divide by 24 hours
-  return (start - end) / oneDay;
+	// so it's safe to divide by 24 hours
+	return (start - end) / oneDay;
 }
 
 //const blankName = fetch("./example.json")
@@ -70,35 +86,39 @@ function DaysBetween(StartDate, EndDate) {
 const appID = new URLSearchParams(window.location.search).get("appID");
 //var variable = blankName?.find(item => item.appID === toString(appID))
 const errorMessages = [
-  "yo lookie here theres an error:",
-  "yo theres an error right here:",
-  "must have slipped through:",
+	"yo lookie here theres an:",
+	"yo theres an error right here:",
+	"must have slipped through:",
 ];
 (async () => {
-  try {
-    const jsonData = await readFile("./actualexample.json");
-    console.log(jsonData.data.history);
-    for (x in jsonData.data.history) {
-      const currentSaleHistory = jsonData.data.history[x];
-      console.log(
-        `${new Intl.DateTimeFormat(undefined, { dateStyle: "full" }).format(
-          new Date(currentSaleHistory.x)
-        )} → Inital Price: $${roundToNearestTenth(
-          calculateInitialPrice(currentSaleHistory.y, currentSaleHistory.d)
-        )}, Current Price: ${currentSaleHistory.f}${returnSaleName(
-          jsonData.data.sales[currentSaleHistory.x]
-        )}`
-      );
-    }
-    console.log(
-      DaysBetween(
-        jsonData.data.history[jsonData.data.history.length - 2].x,
-        jsonData.data.history[jsonData.data.history.length - 1].x
-      )
-    );
-  } catch (error) {
-    console.error(errorMessages[Math.floor(Math.random() * 3)], error);
-  }
+	try {
+		if (appID == undefined) {
+			console.log("No appID provided, cannot look for app.");
+			return;
+		}
+
+		const steamData = await fetchData(`test/steam/${appID}.json`); // change to ` https://store.steampowered.com/api/appdetails?appids=${appID}`
+		if (steamData.ok == false) {
+			throw new Error(`Could not fetch data from Steam`);
+		} else if (steamData[appID].success == false) {
+			throw new Error("AppID not found on Steam, must be invalid.");
+		}
+
+    document.getElementById("gameDetails").innerText += `${steamData[appID].data.name} (${appID})`;
+		if (steamData[appID].data.is_free == true) {
+			return console.log(`${steamData[appID].data.name} is a free game, no need for sale guessing.`);
+		}
+		const priceHistory = await fetchData(`./test/priceHistory/${appID}.json`); // change to `https://steamdb.info/GetPriceHistory/?appid=${appID}&cc=us`
+    const saleData = checkForLastSale(priceHistory);
+	if (!JSON.stringify(saleData).includes('saleDuration')) {
+		return saleData;
+	}
+    document.getElementById('estimatedPrice').innerText = document.getElementById('estimatedPrice').innerText.replace('$0.00', `$${saleData["priceEstimation"]}`);
+    // document.getElementById('nextSale').innerText += `${styleDate(new Date(new Date().setDate(new Date().getDate() + saleData.saleDuration)))}`
+
+	} catch (error) {
+		console.error(error);
+	}
 })();
 
 // console.log(fetch("./413150/"))
